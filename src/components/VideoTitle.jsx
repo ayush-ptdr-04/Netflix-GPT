73d30cf826565c8ceeb392cd1cf7a0f88be769db
aayush patidar <ap6887980@gmail.com>
2025-04-22 17:09:20 +0530

    put styling on the trailer

--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="pt-36 px-12">
+    <div className="w-screen aspect-video bg-gradient-to-r from-black pt-[20%] px-24 absolute text-white">
       <h1 className="font-bold text-6xl">{title}</h1>
       <p className="text-lg py-6 w-1/4">{overview}</p>
       <div>
-        <button className="p-4 px-12 text-xl bg-gray-500 opacity-50 text-white rounded-lg">
+        <button className="p-4 px-12 text-xl bg-white  text-black rounded-lg hover:opacity-80">
           ▶️ Play
         </button>
-        <button className="mx-2 p-4 px-12 text-xl bg-gray-500 opacity-50 text-white rounded-lg">
+        <button className="mx-2 p-4 px-12 text-xl bg-gray-500 opacity-60 hover:opacity-90 text-white rounded-lg">
           More Info
         </button>
       </div>
